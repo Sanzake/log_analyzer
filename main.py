6bac12d91dd4d76ec9c3e9240940acfcfdaa5cd4
Daniel <danhab20@gmail.com>
2026-02-10 11:42:00 +0200

    create func to get sensitive ports

--- main.py
+++ main.py
@@ -13,12 +13,15 @@ def get_out_ips(data):
     out_ips = [log[1] for log in data if not log[1].startswith(("10.", "192.168."))]
     return out_ips
 
+def get_sensitive_ports(data):
+    sensitive_ports = [log for log in data if int(log[3]) in [22,33,3389]]
+    return sensitive_ports
+
 
 def main():
     data = get_logs(logs)
-    for i in get_out_ips(data):
-        print(i)
-
+    out_ips = get_out_ips(data)
+    sensitive_ports = get_sensitive_ports(data)
 
 if __name__ == "__main__":
     main()
